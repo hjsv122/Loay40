667ae83aca5c6e641bdb7e859804222f5224a554
hjsv122 <lwyalftal@gmail.com>
2025-08-22 02:10:19 +0300

    Create app.js

---
+++ contract/backend/frontend/app.js
@@ -0,0 +1,8 @@
+console.log("FlatDrive 2.1 frontend loaded");
+
+const balancesDiv = document.getElementById("balances");
+
+document.getElementById("start").onclick = () => { console.log("بدأت اللعبة"); };
+document.getElementById("stop").onclick = () => { console.log("توقفت اللعبة"); };
+document.getElementById("collect").onclick = () => { console.log("تم جمع القطع"); };
+document.get
